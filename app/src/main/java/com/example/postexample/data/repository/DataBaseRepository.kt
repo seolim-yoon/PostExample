c74478ce830666ea8b15cec513169c1b66bf5533
Dongle <dongle@woowahan.com>
2021-05-13 14:57:48 +0900

    - Return type add

--- app/src/main/java/com/example/postexample/data/repository/DataBaseRepository.kt
+++ app/src/main/java/com/example/postexample/data/repository/DataBaseRepository.kt
@@ -10,8 +10,8 @@ class DataBaseRepository {
     private val firebaseAuth = FirebaseAuth.getInstance()
     private val databaseReference = FirebaseDatabase.getInstance().reference
 
-    fun doSignUp(name: String, email: String, pw: String) =
-        Single.create<AuthResult> { singleEmitter ->
+    fun doSignUp(name: String, email: String, pw: String): Single<AuthResult> =
+        Single.create { singleEmitter ->
             firebaseAuth.createUserWithEmailAndPassword(email, pw)
                 .addOnCompleteListener { task ->
                     with(task) {
@@ -31,8 +31,8 @@ class DataBaseRepository {
                 }
         }
 
-    fun doLogIn(email: String, pw: String) =
-        Single.create<AuthResult> { singleEmitter ->
+    fun doLogIn(email: String, pw: String): Single<AuthResult> =
+        Single.create { singleEmitter ->
             firebaseAuth.signInWithEmailAndPassword(email, pw).addOnCompleteListener { task ->
                 task.takeIf { it.isSuccessful }?.let {
                     singleEmitter.onSuccess(it.result)
